9213f020c29392e95f3da410b6527afb5d0f1d7a
Lina_Razouk99 <47284867+99lina@users.noreply.github.com>
2024-03-06 17:26:48 +0000

    make it general

--- genre-classifier-snorkel/src/run_mlp.py
+++ genre-classifier-snorkel/src/run_mlp.py
@@ -4,7 +4,19 @@ from tira.third_party_integrations import ir_datasets, get_output_directory
 from __init__ import CLASSIFIER_PATH
 import pandas as pd
 from pathlib import Path
+import enum
  
+class label(enum.Enum):
+    # Constants for the labels
+    ABSTAIN = -1
+    ARTICLES = 0
+    DISCUSSION = 1
+    DOWNLOAD = 2
+    HELP = 3
+    LINKLISTS = 4
+    PROTAIT_NPRIV = 5
+    PORTRAIT_PRIV = 6
+    SHOP = 7
 
 def parse_args():
     import argparse
@@ -35,21 +47,24 @@ if __name__ == '__main__':
     args = parse_args()
 
     # to classify gerneral dataset uncomment this lines and comment the lines ab 47
-    # dataset = ir_datasets.load(args.input)
-    # data = get_df_text_for_ir_dataset(dataset)
-    # print(data)
-    # res = classify(data)
-    # output_dir = get_output_directory('.')
-    # output_file = Path(output_dir) / 'documents_newmlp.jsonl.gz'
-    # pd.DataFrame(data).to_json(output_file, lines=True, orient='records')
-
-    # test the model on the labeled test data from zenodo dataset
-    _, dataset_test_data = load_plain_text_dfs()
-    data = dataset_test_data
-    data = classify(data, text_col='plain_text', text_type='plain_text')
-    res = pd.DataFrame(zip(data['file_name'], data['label'], data['predicted_labels']))
-    print(f'Accuracy on test data: '
-          f'{data.loc[data["label"] == data["predicted_labels"]].shape[0] / data.shape[0]}')
+    dataset = ir_datasets.load(args.input)
+    data = get_df_text_for_ir_dataset(dataset)
+    res = classify(data)
+    res = pd.DataFrame(zip(data['docno'],
+                          [label(i).name for i in data['predicted_labels']]))
     output_dir = get_output_directory('.')
     output_file = Path(output_dir) / 'documents_newmlp.jsonl.gz'
     pd.DataFrame(res).to_json(output_file, lines=True, orient='records')
+
+    # test the model on the labeled test data from zenodo dataset
+    # _, dataset_test_data = load_plain_text_dfs()
+    # data = dataset_test_data
+    # data = classify(data, text_col='plain_text', text_type='plain_text')
+
+    # res = pd.DataFrame(zip(data['file_name'], [label(i).name for i in data['label']], 
+    #                        [label(i).name for i in data['predicted_labels']]))
+    # print(f'Accuracy on test data: '
+    #       f'{data.loc[data["label"] == data["predicted_labels"]].shape[0] / data.shape[0]}')
+    # output_dir = get_output_directory('.')
+    # output_file = Path(output_dir) / 'documents_newmlp.jsonl.gz'
+    # pd.DataFrame(res).to_json(output_file, lines=True, orient='records')
