f514c3c4c99b7fb5f8537425164da1cb8b59d32c
Lina_Razouk99 <47284867+99lina@users.noreply.github.com>
2024-03-06 17:13:05 +0000

    run test dataset on mlp

--- genre-classifier-snorkel/src/run_mlp.py
+++ genre-classifier-snorkel/src/run_mlp.py
@@ -1,4 +1,5 @@
 from joblib import load
+from load_data import load_plain_text_dfs
 from tira.third_party_integrations import ir_datasets, get_output_directory
 from __init__ import CLASSIFIER_PATH
 import pandas as pd
@@ -11,31 +12,44 @@ def parse_args():
     parser.add_argument('--input', type=str, default='workshop-on-open-web-search/document-processing-20231027-training', help='Dataset id from ir_datasets/TIRA to process.')
     return parser.parse_args()
 
-def classify(data, modelname='mlp', language='english', text_col='plain_text'):
+def classify(data, modelname='mlp', language='english', text_col='text', text_type='plain_text'):
     
-    vectorizer = load(CLASSIFIER_PATH / f"vectorizer_{text_col}_{modelname}_{language}.joblib")
-    mlp_classifier = load(CLASSIFIER_PATH / f"classifier_{text_col}_{modelname}_{language}.joblib")
+    vectorizer = load(CLASSIFIER_PATH / f"vectorizer_{text_type}_{modelname}_{language}.joblib")
+    mlp_classifier = load(CLASSIFIER_PATH / f"classifier_{text_type}_{modelname}_{language}.joblib")
 
-    v_data = vectorizer.transform(data['text'])
+    v_data = vectorizer.transform(data[text_col])
     predicted_labels = mlp_classifier.predict(v_data)
-    data['labels'] = predicted_labels
+    data['predicted_labels'] = predicted_labels
     return data
 
-def get_df_text(dataset):
-    doc = {}
+def get_df_text_for_ir_dataset(dataset):
     data = []
     for i in dataset.docs_iter():
         data += [{'docno': i.doc_id, 'text': i.default_text()}]
     res = pd.DataFrame(data)
     return res
 
+
 if __name__ == '__main__':
     print("start the test")
     args = parse_args()
-    dataset = ir_datasets.load(args.input)
-    data = get_df_text(dataset)
-    print(data)
-    res = classify(data)
+
+    # to classify gerneral dataset uncomment this lines and comment the lines ab 47
+    # dataset = ir_datasets.load(args.input)
+    # data = get_df_text_for_ir_dataset(dataset)
+    # print(data)
+    # res = classify(data)
+    # output_dir = get_output_directory('.')
+    # output_file = Path(output_dir) / 'documents_newmlp.jsonl.gz'
+    # pd.DataFrame(data).to_json(output_file, lines=True, orient='records')
+
+    # test the model on the labeled test data from zenodo dataset
+    _, dataset_test_data = load_plain_text_dfs()
+    data = dataset_test_data
+    data = classify(data, text_col='plain_text', text_type='plain_text')
+    res = pd.DataFrame(zip(data['file_name'], data['label'], data['predicted_labels']))
+    print(f'Accuracy on test data: '
+          f'{data.loc[data["label"] == data["predicted_labels"]].shape[0] / data.shape[0]}')
     output_dir = get_output_directory('.')
     output_file = Path(output_dir) / 'documents_newmlp.jsonl.gz'
-    pd.DataFrame(data).to_json(output_file, lines=True, orient='records')
+    pd.DataFrame(res).to_json(output_file, lines=True, orient='records')
--- genre-classifier-snorkel/tests/test_mlp.py
+++ genre-classifier-snorkel/tests/test_mlp.py
@@ -1,1 +1,6 @@
-# TODO 
+import unittest
+
+class mlp_test(unittest.TestCase):
+    def test_super_strict(self):
+        # a super strict test :)
+        self.assertEqual(1, 1)
